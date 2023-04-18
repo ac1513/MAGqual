26a0c86835a8bc09d0bc2726d8c0b264a505686d
ac1513 <ac1513@york.ac.uk>
2023-04-18 14:37:49 +0100

    updating variable names

--- MAGqual.py
+++ MAGqual.py
@@ -7,7 +7,7 @@ def_prefix = "MAGqual_" + str(date.today()).replace("-","")
 parser = argparse.ArgumentParser(description='usage = python MAGqual.py -a/--asm assembly.fa -b/--bins bins_dir/ (-j/--jobs 10 -p/--prefix MAGqual_run --cluster slurm --checkmdb checkm_db/ --baktadb bakta_db/)')
 
 parser.add_argument('-a', '--asm', dest='assembly', required = True, help = 'location of the assembly used to generate the bins', type = str)
-parser.add_argument('-b', '--bins', dest='binloc', required = True, help = 'location of the directory containing the bins to run through MAGqual', type = str)
+parser.add_argument('-b', '--bins', dest='bindir', required = True, help = 'location of the directory containing the bins to run through MAGqual', type = str)
 
 parser.add_argument('-p', '--prefix', dest='prefix', help = 'prefix for the MAGqual run, default = MAGqual_YYYYMMDD', type = str, default=def_prefix)
 parser.add_argument('-j', '--jobs', dest='jobs', help = 'number of jobs to be run concurrently, default = 1', type = int, default=1)
@@ -21,7 +21,7 @@ args = parser.parse_args()
 prefix = args.prefix
 jobs = args.jobs
 assembly = args.assembly
-binloc = args.binloc
+bindir = args.bindir
 
 if args.checkmdb:
     checkmdb = args.checkmdb
@@ -35,8 +35,8 @@ else:
     bakta_com = ""
 
 if args.cluster == "slurm":
-    command = "snakemake --use-conda -j " + str(jobs) + " --config JOBID=" + prefix + " ASM_LOC=" + assembly + " BIN_LOC=" + binloc + checkm_com + bakta_com + ' --cluster-config config/cluster.json --cluster "sbatch -A {cluster.account} -p {cluster.partition} -t {cluster.time} -c {cluster.threads} --mem-per-cpu {cluster.mem}"'
+    command = "snakemake --use-conda -j " + str(jobs) + " --config JOBID=" + prefix + " ASM_LOC=" + assembly + " BIN_LOC=" + bindir + checkm_com + bakta_com + ' --cluster-config config/cluster.json --cluster "sbatch -A {cluster.account} -p {cluster.partition} -t {cluster.time} -c {cluster.threads} --mem-per-cpu {cluster.mem}"'
 else:
-    command = "snakemake --use-conda -j " + str(jobs) + " --config JOBID=" + prefix + " ASM_LOC=" + assembly + " BIN_LOC=" + binloc + checkm_com + bakta_com
+    command = "snakemake --use-conda -j " + str(jobs) + " --config JOBID=" + prefix + " ASM_LOC=" + assembly + " BIN_LOC=" + bindir + checkm_com + bakta_com
 
 print(command)
